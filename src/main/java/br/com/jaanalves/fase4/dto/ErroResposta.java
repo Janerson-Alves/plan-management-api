e3e77306ea1356a2da12803bafe8ebd21757ed08
Jaan Alves <djjaan.26@gmail.com>
2026-08-13 16:02:30 -0300

    Feat: implementa handler gloval de excecoes com RestControllerAdvice

---
+++ src/main/java/br/com/jaanalves/fase4/dto/ErroResposta.java
@@ -0,0 +1,52 @@
+package br.com.jaanalves.fase4.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ErroResposta {
+    private LocalDateTime timestamp;
+    private int status;
+    private String erro;
+    private String mensagem;
+
+    // Construtor
+    public ErroResposta(int status, String erro, String mensagem) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.erro = erro;
+        this.mensagem = mensagem;
+    }
+
+    // Getters e Setters
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getErro() {
+        return erro;
+    }
+
+    public void setErro(String erro) {
+        this.erro = erro;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
---
+++ src/main/java/br/com/jaanalves/fase4/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package br.com.jaanalves.fase4.exception;
+
+import br.com.jaanalves.fase4.dto.ErroResposta;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+// Escuta e interceptar os erros disparados na aplicação
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    // Intercepta erros de validação do @Valid (Bean Validation)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErroResposta> tratarErroValidacao(MethodArgumentNotValidException ex) {
+        // Pega todos os erros de campo e formata em uma mensagem legivel
+        String mensagem = ex.getBindingResult().getFieldErrors().stream()
+                .map(erro -> erro.getField() + ": " + erro.getDefaultMessage())
+                .collect(Collectors.joining(" | "));
+        // Instancia um novo erro passanso as informações para ele.
+        ErroResposta erro = new ErroResposta(
+                HttpStatus.BAD_REQUEST.value(),
+                "Erro de Validação nos Dados",
+                mensagem
+        );
+        // Retorna o erro e mostra no body a mensagem de erro
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
+
+}
